662e9713ad58066ad224e4e0f14a34c942c99370
SachinSathyababu <sach.203@gmail.com>
2020-02-08 12:48:23 +0530

    aspectlatest

--- PizzaBunglow/src/main/java/com/mytectra/springboot/PizzaBunglow/aspect/TimingAspect.java
+++ PizzaBunglow/src/main/java/com/mytectra/springboot/PizzaBunglow/aspect/TimingAspect.java
@@ -8,6 +8,8 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
+import com.mytectra.springboot.PizzaBunglow.model.AddOns;
+
 @Aspect
 @Component
 public class TimingAspect {
@@ -24,9 +26,15 @@ public class TimingAspect {
 */
 	/*@Pointcut("execution(* com.mytectra.springboot.PizzaBunglow.Dao.*.*(*)) && args(id)")
 	public void getIdsPC(int id) {
+	}
+	
+	@Before("getIdsPC(id)")
+	public void getInput(int id ) throws Throwable {
+		System.out.println("Arg recived inside aspect- "+ id);
+		
 	}*/
 	
-	@Around("@annotation(com.mytectra.springboot.PizzaBunglow.aspect.Timer)")
+	/*@Around("@annotation(com.mytectra.springboot.PizzaBunglow.aspect.Timer)")
 	public Object evaluateTime(ProceedingJoinPoint jp ) throws Throwable {
 		//System.out.println("Arg recived - "+ id);
 		long t1 = System.currentTimeMillis();
@@ -34,8 +42,18 @@ public class TimingAspect {
 		long t2 = System.currentTimeMillis();
 		System.out.println( "Time taken to excute " + jp + "  = "  + ((t2 - t1)/1000) + "secs");
 		return result;
+	}*/
+	
+	@Pointcut("execution(void com.mytectra.springboot.PizzaBunglow.Dao.AddOnsDao.saveAddOns(..))")
+	public void trimingAddonsNameAdonsDao() {
 	}
     
+	@Before("trimingAddonsNameAdonsDao() && args(addons)")
+	public void getInput(AddOns addons) throws Throwable {
+		System.out.println("Name of addons- "+ addons.getName());
+		addons.setName(addons.getName().trim()+"Sachin");
+		
+	}
 
 }
 
